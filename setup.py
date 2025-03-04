cbf40bfe71aa52c3a9fa743a8dab6eb2237935e3
German Margon <43929286+arsnovo@users.noreply.github.com>
2025-03-04 21:48:53 +0100

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="midiscript",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     author="arsnovo",
     description="Programming language for musicians",
