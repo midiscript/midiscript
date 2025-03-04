d1345aefe4f87537a0b9fe129b7354f8272b8873
German Margon <43929286+arsnovo@users.noreply.github.com>
2025-03-04 23:36:54 +0100

    Fix/pypi workflow script v7 (#22)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="midiscript",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     author="arsnovo",
     description="Programming language for musicians",
