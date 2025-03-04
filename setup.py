590019be73cc80d3edbf7c50a9d17c1dc2906dd8
German Margon <43929286+arsnovo@users.noreply.github.com>
2025-03-04 21:51:08 +0100

    Fix setup.py merge conflicts and bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -10,4 +10,21 @@ setup(
     install_requires=[
         "midiutil>=1.2.1",
     ],
+    entry_points={
+        "console_scripts": [
+            "midiscript=midiscript.cli:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Musicians",
+        "Topic :: Multimedia :: Sound/Audio :: MIDI",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
