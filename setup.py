ad88beebe6c3c13f999e5bad9d9b9e1e5d766510
German Margon <43929286+arsnovo@users.noreply.github.com>
2025-03-04 23:33:38 +0100

    Fix release version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="midiscript",
     version="0.2.3",
+    packages=find_packages(),
     author="arsnovo",
     description="Programming language for musicians",
-    packages=find_packages(),
-    python_requires=">=3.9",
+    python_requires=">=3.6",
     install_requires=[
         "midiutil>=1.2.1",
     ],
@@ -21,9 +21,10 @@ setup(
         "Intended Audience :: Musicians",
         "Topic :: Multimedia :: Sound/Audio :: MIDI",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
 )
