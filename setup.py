bd6ce5bc268125fc547daedf4926d204cf01d3b9
German Margon <43929286+arsnovo@users.noreply.github.com>
2025-03-04 23:07:06 +0100

    Bump version to 0.2.2 and fix PyPI workflow script

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="midiscript",
-    version="0.2.1",
-    packages=find_packages(),
+    version="0.2.2",
     author="arsnovo",
     description="Programming language for musicians",
-    python_requires=">=3.6",
+    packages=find_packages(),
+    python_requires=">=3.11",
     install_requires=[
         "midiutil>=1.2.1",
     ],
+    entry_points={
+        "console_scripts": [
+            "midiscript=midiscript.cli:main",
+        ],
+    },
 )
